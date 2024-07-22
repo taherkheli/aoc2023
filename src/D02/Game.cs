144caace7c835d4f980042d9103634ae61841921
Bilal Taherkheli <bilal76@gmail.com>
2024-07-22 10:30:06 +0200

    D2 completed

---
+++ src/D02/Game.cs
@@ -0,0 +1,37 @@
+﻿namespace aoc.D02
+{
+  public class Game(Turn[] turns)
+  {
+    private readonly Turn[] _turns = turns;
+
+    public bool Possible => GetPossibility(); 
+
+    public int Power => GetPower();
+
+    private bool GetPossibility()
+    {
+      return _turns.All(t => t.red < 13 && t.green < 14 && t.blue < 15);
+    }
+
+    private int GetPower() 
+    {
+      int maxRed = 0;
+      int maxGreen = 0;
+      int maxBlue = 0;
+
+      foreach (var turn in _turns)
+      {
+        if (turn.red > maxRed)
+          maxRed = turn.red;
+
+        if (turn.green > maxGreen)
+          maxGreen = turn.green;
+
+        if (turn.blue > maxBlue)
+          maxBlue = turn.blue;
+      }
+
+      return maxRed * maxGreen * maxBlue;
+    }
+  }
+}
---
+++ src/D02/Parser.cs
@@ -0,0 +1,70 @@
+﻿namespace aoc.D02
+{
+  public static class Parser
+  {
+    public static int Parse(string path, bool isPartII = false)
+    {
+      var lines = File.ReadAllLines(path);
+      int sum = 0;
+      int power = 0;
+
+      for (int i = 0; i < lines.Length; i++)    
+      { 
+        var game = new Game(GetTurns(lines[i]));
+        power += game.Power; 
+
+        if (game.Possible)        
+          sum += i + 1;         //game id = i + 1
+      }
+
+      if (isPartII)
+        return power;
+      else 
+        return sum;
+    }
+
+    private static Turn[] GetTurns(string s)
+    {
+      var s_array = s.Split(':', StringSplitOptions.TrimEntries);  // discard game part
+      s = s_array[1];
+      var turns = s.Split(';', StringSplitOptions.TrimEntries);  // retrieve number of turns in this game
+
+      var result = new Turn[turns.Length];
+
+      for (int i = 0; i < turns.Length; i++)
+      {
+        var configuration = turns[i].Split(',', StringSplitOptions.TrimEntries);
+        int reds = 0;
+        int greens = 0;
+        int blues = 0;
+
+        foreach (var c in configuration)
+        {
+          var temp = c.Split(' ', StringSplitOptions.TrimEntries);
+          var color = temp[1];
+          var num = int.Parse(temp[0]);
+
+          switch (color)
+          {
+            case "red":
+              reds = num;
+              break;
+            case "green":
+              greens = num;
+              break;
+            case "blue":
+              blues = num;
+              break;
+            default:
+              throw new Exception("Should not happen!");
+          }
+        }
+
+        result[i] = new Turn(reds, blues, greens);
+      }
+
+      return result;
+    }
+
+  }
+}
