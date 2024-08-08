4dce089efceaf838044fc9ef02e3d2b6520343bb
Bilal Taherkheli <taherkheli@users.noreply.github.com>
2024-08-08 12:39:41 +0200

    D4 completed. Pretty inefficient at about 20s for partII but at least works (#3)

---
+++ src/D04/Deck.cs
@@ -0,0 +1,72 @@
+﻿namespace aoc.D04
+{
+  public class Deck
+  {
+    private List<Card> _cards;
+
+    public Deck(List<Card> cards)
+    {
+      _cards = cards;
+    }
+
+    public List<Card>Cards { get { return _cards; } }
+
+    public int SolvePartI()
+    {
+      int sum = 0;
+
+      foreach (var card in _cards)
+      {
+        int points = 0;
+        var intersection = card.Numbers[0].Intersect(card.Numbers[1]).ToArray();
+
+        if (intersection.Length > 0)
+        {
+          points = 1;
+
+          for (int i = 1; i < intersection.Length; i++)  //start at index 1 as first match counts 1 point only 
+            points = 2 * points;
+        }
+
+        sum += points;
+      }
+
+      return sum;
+    }
+
+    public int SolvePartII()
+    {
+      int[] matchCounts = new int[_cards.Count];
+      List<Card> copies = new List<Card>();
+
+      for (int i = 0; i < _cards.Count; i++)
+      {
+        var intersection = _cards[i].Numbers[0].Intersect(_cards[i].Numbers[1]).ToArray();
+        matchCounts[i] = intersection.Length;
+      }
+
+      for (int i = 0; i < matchCounts.Length; i++)
+      {
+        int id = i + 1;
+
+        // find instance count of this id
+        var currentCount = _cards.Where(c => c.ID == id).ToList().Count;
+
+        for (int j = 0; j < currentCount; j++)  //repeat for each instance        
+        {
+          for (int k = id + 1; k < id + matchCounts[i] + 1; k++)  //create copies with correct ID
+          {
+            var first = _cards.First(c => c.ID == k);
+            copies.Add(new Card(k, first.Numbers));
+          }
+        }
+
+        // update cards by appending copies and reset copies
+        _cards.AddRange(copies);
+        copies = new List<Card>();
+      }
+
+      return _cards.Count;
+    }
+  }
+}
---
+++ src/D04/Parser.cs
@@ -0,0 +1,34 @@
+﻿namespace aoc.D04
+{
+  public static class Parser
+  {
+    public static Deck Parse(string path)
+    {
+      int[] cardNumbers;
+      int[] yourNumbers;
+      var lines = File.ReadAllLines(path);
+      int index = 1;
+      List<Card> cards = new List<Card>();
+
+      foreach (var line in lines)
+      {
+        var data = line.Split(':', StringSplitOptions.TrimEntries)[1].Split('|', StringSplitOptions.TrimEntries);
+        var cardData = data[0].Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        var numbersData = data[1].Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        cardNumbers = new int[cardData.Length];
+        yourNumbers = new int[numbersData.Length];
+
+        for (int i = 0; i < cardNumbers.Length; i++)
+          cardNumbers[i] = int.Parse(cardData[i]);
+
+        for (int i = 0; i < yourNumbers.Length; i++)
+          yourNumbers[i] = int.Parse(numbersData[i]);
+
+        cards.Add(new Card(index, new int[][] { cardNumbers, yourNumbers }));
+        index++;
+      }
+
+      return new Deck(cards);
+    }
+  }
+}
